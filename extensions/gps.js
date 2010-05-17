a9119540f41dbaf66b2268868efebc8ff14e4dcd
skunk <skunk@7be66250-f027-0410-bdcb-c60b8b17d9db>
2010-05-17 21:41:25 +0000

    Extension GPS: Remember last zoom-level and base-layer
    git-svn-id: file:///home/skunk/svn/photos@112 7be66250-f027-0410-bdcb-c60b8b17d9db

--- extensions/gps.js
+++ extensions/gps.js
@@ -1,5 +1,13 @@
 var gps_map;
 
+function gps_zoom_change(event) {
+  options_set("gps_zoom", gps_map.zoom);
+}
+
+function gps_baselayer_change(event) {
+  options_set("gps_baselayer", gps_map.baseLayer.id);
+}
+
 function gps_init() {
   var map=document.getElementById("gps_map");
 
@@ -15,19 +23,28 @@ function gps_init() {
                         new OpenLayers.Control.Navigation() ]
           });
 
-  var l= new OpenLayers.Layer.Google( "Google", { type: G_HYBRID_MAP,
-     projection: new OpenLayers.Projection("EPSG:4326"), 'sphericalMercator': true } );
+  var l = new OpenLayers.Layer.OSM("OpenStreetBrowser", "http://www.openstreetbrowser.org/tiles/base/${z}/${x}/${y}.png", {numZoomLevels: 19});
   gps_map.addLayer(l);
 
   var l = new OpenLayers.Layer.OSM.Mapnik("OpenStreetMap - Mapnik");
   gps_map.addLayer(l);
 
-  var l = new OpenLayers.Layer.OSM("OpenStreetBrowser", "http://www.openstreetbrowser.org/tiles/base/${z}/${x}/${y}.png", {numZoomLevels: 19});
+  var l= new OpenLayers.Layer.Google( "Google", { type: G_HYBRID_MAP,
+     projection: new OpenLayers.Projection("EPSG:4326"), 'sphericalMercator': true } );
   gps_map.addLayer(l);
 
- 
+  var z=options_get("gps_baselayer");
+  for(var i=0; i<gps_map.layers.length; i++) {
+    if(gps_map.layers[i].id==z)
+      gps_map.setBaseLayer(gps_map.layers[i]);
+  }
+
+  var z=options_get("gps_zoom");
+  if(!z)
+    z=13;
+
   var gps_pos=new OpenLayers.LonLat(map.getAttribute("pos_lon"), map.getAttribute("pos_lat")).transform(new OpenLayers.Projection("EPSG:4326"), gps_map.getProjectionObject());
-  gps_map.setCenter(gps_pos, 13);
+  gps_map.setCenter(gps_pos, z);
 
   var markers = new OpenLayers.Layer.Markers( "Markers" );
   gps_map.addLayer(markers);
@@ -47,6 +64,10 @@ function gps_init() {
   var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
   var icon = new OpenLayers.Icon('http://www.openstreetmap.org/openlayers/img/marker.png',size,offset);
   markers.addMarker(new OpenLayers.Marker(gps_pos,icon));
+
+  gps_map.events.register("zoomend", gps_map, gps_zoom_change);
+  gps_map.events.register("changebaselayer", gps_map, gps_baselayer_change);
+
 }
 
 register_initfun(gps_init);
--- extensions/gps_data.php
+++ extensions/gps_data.php
@@ -1,3 +1,3 @@
 <?
-set_extension_views("gps", array("imageview"));
+set_extension_views("gps", array("imageview", "options"));
 set_extension_description("gps", "GPS", "Reads GPS-Information of geotagged images");
